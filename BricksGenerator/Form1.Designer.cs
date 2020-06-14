3c86ab1ec2a5263fa42ae1569c88058bda64be70
LeoWang <wwieo055169@gmail.com>
2020-06-15 00:26:28 +0800

    Initial commit

--- BricksGenerator/Form1.Designer.cs
+++ BricksGenerator/Form1.Designer.cs
@@ -28,20 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bk_casual = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bk_casual)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BricksGenerator.Properties.Resources.bg;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::BricksGenerator.Properties.Resources.bg;
+            this.pictureBox2.Location = new System.Drawing.Point(360, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(140, 690);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bk_casual
+            // 
+            this.bk_casual.Image = global::BricksGenerator.Properties.Resources.casual;
+            this.bk_casual.Location = new System.Drawing.Point(390, 23);
+            this.bk_casual.Name = "bk_casual";
+            this.bk_casual.Size = new System.Drawing.Size(80, 15);
+            this.bk_casual.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bk_casual.TabIndex = 2;
+            this.bk_casual.TabStop = false;
+            this.bk_casual.Click += new System.EventHandler(this.bk_casual_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(492, 693);
+            this.Controls.Add(this.bk_casual);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(510, 740);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BricksGenerator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bk_casual)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox bk_casual;
     }
 }
 
